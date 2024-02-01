cd2962cb2a6594da6cc7c8ec348c59d82cef497f
xocamy <131361667+xocamy@users.noreply.github.com>
2024-01-31 20:00:53 -0800

    Update index.js
    Signed-off-by: xocamy <131361667+xocamy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.set("view engine", "ejs")
 
-app.use(express.static("./public"));
+app.use(express.static("./public_html"));
 app.use("/uv/", express.static(uvPath));
 app.use("/dynamic/", express.static(dynamicPath))
 
